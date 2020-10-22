36b71c06a756d86397328f31c2418e5ca1f807ae
adarshkumaruppala <36727247+adarshkumaruppala@users.noreply.github.com>
2020-10-22 15:16:54 +0530

    Add files via upload
    CIRCULAR LINKED LIST

--- Node.java
+++ Node.java
@@ -1,15 +1,10 @@
-package ds.BST;
-
+package ds.circularLinkedList;
 public class Node {
-	
-	int key;
-	String name;
-	Node left,right;
-	public Node(int key, String name) {
-		super();
-		this.key = key;
-		this.name = name;
+	//int data;
+	public Node next;
+	public  int data;
+    
+	public void displayNode() {
+		System.out.println("{ "+data+" } ");
 	}
-	
-
 }
